fb96b46c6428ab0876193a268cba2f8109f22145
Shashikant Patil <shashikantpatil2727@outlook.com>
2024-04-17 13:41:42 +0530

    commit

--- provider.tf
+++ provider.tf
@@ -1,13 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  
+  subscription_id = "7b2b23f1-fac4-47ad-9bf9-3621c01e8d76"
+  client_id       = "24471f24-05dc-4e83-884a-cf1df5de265b"
+  client_secret   = "wMZ8Q~DJ27fcqAqKkEJ1oPJn53q1pwGHMzxDTdmW"
+  tenant_id       = "1ba23728-0068-485c-9887-82f19d0efba1"
 }
